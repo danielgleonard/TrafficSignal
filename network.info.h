b33f04ccbb27c33b29f91e69a1fef03a7bc855f1
Dan Leonard <daniel@danleonard.us>
2018-12-31 22:42:10 -0600

    Prepared network connection code

---
+++ network.info.h
@@ -0,0 +1,8 @@
+#ifndef NETWORK_INFO_H
+#define NETWORK_INFO_H
+
+char net_ssid[];
+char net_password[];
+int keyIndex; 
+
+#endif
---
+++ network_connection.ino
@@ -0,0 +1,24 @@
+#include <SPI.h>
+#include <WiFi.h>
+#include "network.info.h"
+
+int status = WL_IDLE_STATUS;
+
+WiFiServer server(80);
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
