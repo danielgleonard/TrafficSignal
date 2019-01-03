d060745bcd2cce0732bdb9a8d4bf6535b83ae537
Dan Leonard <daniel@danleonard.us>
2019-01-02 21:00:01 -0600

    network type decider thing

--- network_connection.ino
+++ network_connection.ino
@@ -1,10 +1,57 @@
 #include <SPI.h>
 #include <WiFi.h>
-#include "network.info.h"
+#include "network_info.h"
 
 int status = WL_IDLE_STATUS;
 
 WiFiServer server(80);
+
+bool connectWifi() {
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    return false;
+  } else {
+    String fv = WiFi.firmwareVersion();
+    if (fv != "1.1.0") {
+      Serial.println("Please upgrade the firmware");
+    }
+
+    /*
+     * Connect to network
+     * Performs different procedure based on NETWORK_TYPE
+     * using #if directives to save space on microcontroller
+     */
+#if NETWORK_TYPE == 0
+
+#elif NETWORK_TYPE == 1
+    // attempt to connect to Wifi network:
+    while (status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to WEP network, SSID: ");
+      Serial.println(ssid);
+      status = WiFi.begin(ssid, keyIndex, key);
+  
+      // wait 10 seconds for connection:
+      delay(10000);
+    }
+#elif NETWORK_TYPE == 2
+    // attempt to connect to Wifi network:
+    while (status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to SSID: ");
+      Serial.println(ssid);
+      // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+      status = WiFi.begin(ssid, pass);
+  
+      // wait 10 seconds for connection:
+      delay(10000);
+    }
+    server.begin();
+    // you're connected now, so print out the status:
+    printWifiStatus();      
+#else
+    return false;
+#endif
+  }
+}
 
 void printWifiStatus() {
   // print the SSID of the network you're attached to:
--- network_info.h
+++ network_info.h
@@ -1,8 +1,16 @@
 #ifndef NETWORK_INFO_H
 #define NETWORK_INFO_H
 
+// your network connection protocol (0 = open, 1 = WEP, 2 = WPA2 Personal)
+#define NETWORK_TYPE 0
+
 char net_ssid[] = "";       // your network SSID (name)
+
+// WEP
+char key[] = "";            // your network key
+int keyIndex = 0;           // your network key Index number
+
+// WPA2 Personal
 char net_password[] = "";   // your network password
-int keyIndex = 0;           // your network key Index number (needed only for WEP)
 
 #endif
