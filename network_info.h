07e79f9af2dd57fddf5939709e48ad542fbf6bd2
Dan Leonard <daniel@danleonard.us>
2019-01-03 00:38:54 -0600

    last ‘char’ turned into ‘uint8_t’

--- network_info.h
+++ network_info.h
@@ -13,15 +13,15 @@
 // your network connection protocol (0 = open, 1 = WEP, 2 = WPA2 Personal)
 #define NETWORK_TYPE 0
 
-char net_ssid[] = "";       // your network SSID (name)
+uint8_t net_ssid[] = "";        // your network SSID (name)
 
 #if NETWORK_TYPE == 1
   // WEP
-  char net_key[] = "";        // your network key
-  int net_keyIndex = 0;       // your network key Index number
+  uint8_t net_key[] = "";       // your network key
+  int net_keyIndex = 0;         // your network key Index number
 #elif NETWORK_TYPE == 2
   // WPA2 Personal
-  char net_password[] = "";   // your network password
+  uint8_t net_password[] = "";  // your network password
 #endif
 
 #endif
