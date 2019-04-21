a8bd89a96548a5d42dc152f7fc799d267025bc0e
Dan Leonard <daniel@danleonard.us>
2019-04-20 20:33:09 -0500

    formatted messages???

--- microcontroller/esp8266/esp8266.ino
+++ microcontroller/esp8266/esp8266.ino
@@ -55,48 +55,164 @@ void loop()
 
   if (!client.connected()) {
     client = server.available();
-    digitalWrite(0,LOW);
+    digitalWrite(0, LOW);
   } else {
-    digitalWrite(0,HIGH);
-    readMessages();
+    digitalWrite(0, HIGH);
+    
+    if(readMessages()) {
+      processMessages();
+      sendMessages();
+    }
 
-    while (client.available() > 0) {
-      // Read incoming message
-      char inChar = client.read();
-      // Echo input on Serial monitor
-      Serial.write(inChar);
-    }
-    while (Serial.available() > 0) {
-      char outChar = Serial.read();
-      client.write(outChar);
-    }
+    /*
+        while (client.available() > 0) {
+          // Read incoming message
+          char inChar = client.read();
+          // Echo input on Serial monitor
+          Serial.write(inChar);
+        }
+        while (Serial.available() > 0) {
+          char outChar = Serial.read();
+          client.write(outChar);
+        }
+    */
   }
 }
 
-void readMessage()
+bool readMessages()
 {
+  bool readanything = false;
 
-	// Read message from TCP/IP
-	while (client.available() > 0) {
-		char inChar = client.read();
-		int i = 0;
+  // Read message from TCP/IP
+  if (client.available() > 0) {
+    memset(messageWiFiIn,0,sizeof(messageWiFiIn));
+    memset(messageSerialIn,0,sizeof(messageSerialIn));
+    
+#if DEBUG
+    Serial.write("client.availabile: ");
+    Serial.print(client.available());
+    Serial.write("\r\n");
+#endif
+    
+    char inChar = client.read();
+    int i = 0;
 
-		while (inChar != '\r' && inChar != '\n' && strlen(messageWiFiIn) < sizeof(messageWiFiIn) - 1) {
-			messageWiFiIn[i] = inChar;
-			i++;
-			inChar = client.read();
-		}
-		if (strlen(messageWiFiIn) == sizeof(messageWiFiIn) - 1) {
-		}
-	}
+    // Fill messageWiFiIn with client.read()
+    while (inChar != '\r' && inChar != '\n' && strlen(messageWiFiIn) < sizeof(messageWiFiIn) - 1) {
+      messageWiFiIn[i] = inChar;
+      i++;
+      inChar = client.read();
+    }
+    
+    // dump rest of buffer if there's more
+    if (strlen(messageWiFiIn) == sizeof(messageWiFiIn) - 1 || inChar == '\r') {
+      while (inChar != '\n') {
+        inChar = client.read();
+      }
+    }
+    
+#if DEBUG
+    Serial.write("messageWiFiIn:\"");
+    Serial.write(messageWiFiIn);
+    Serial.write("\" (start: ");
+    Serial.print(messageWiFiIn[0]);
+    Serial.write(")\r\n");
+#endif
+
+    readanything = true;
+  }
+
+  // Read message from Serial
+  if (Serial.available() > 0) {
+    memset(messageWiFiIn,0,sizeof(messageWiFiIn));
+    memset(messageSerialIn,0,sizeof(messageSerialIn));
+
+#if DEBUG
+    Serial.write("serial.availabile: ");
+    Serial.print(Serial.available());
+    Serial.write("\r\n");
+#endif
+
+    char inChar = Serial.read();
+    int i = 0;
+
+    // Fill messageWiFiIn with client.read()
+    while (inChar != '\r' && inChar != '\n' && strlen(messageSerialIn) < sizeof(messageSerialIn) - 1) {
+      messageSerialIn[i] = inChar;
+      i++;
+      inChar = Serial.read();
+    }
+
+    // dump rest of buffer if there's more
+    if (strlen(messageSerialIn) == sizeof(messageSerialIn) - 1 || inChar == '\r') {
+      while (inChar != '\n') {
+        inChar = Serial.read();
+      }
+    }    
+#if DEBUG
+    Serial.write("messageSerialIn:\"");
+    Serial.write(messageSerialIn);
+    Serial.write("\" (start: ");
+    Serial.print(messageSerialIn[0]);
+    Serial.write(")\r\n");
+#endif
+
+    readanything = true;
+  }
+
+#if DEBUG
+    if (readanything) {
+      Serial.write("read things lol\r\n");
+    }
+#endif
+
+  return readanything;
+}
+
+void processMessages() {
+  // check for commands to esp
+  if (messageWiFiIn[0] != '\0' && processCommands(messageWiFiIn) == 2) {
+    memset(messageSerialOut,0,sizeof(messageSerialOut));
+    strncpy(messageSerialOut, messageWiFiIn+5, MESSAGE_SIZE);
+  }
+  if (messageSerialIn[0] != '\0' && processCommands(messageSerialIn) == 2) {
+    memset(messageWiFiOut,0,sizeof(messageWiFiOut));
+    strncpy(messageWiFiOut, messageSerialIn+5, MESSAGE_SIZE);
+  }
+}
+
+char processCommands(char* msg) {
+if (msg[0] == 'e' && msg[1] == 's' && msg[2] == 'p' && msg[3] == ' ') {
+    return 1;
+  } else if (msg[0] == 's' && msg[1] == 'e' && msg[2] == 'n' && msg[3] == 'd' && msg[4] == ' ') {
+#if DEBUG
+    Serial.write("MATCHES \'send\'.\r\n");
+#endif
+    return 2;
+  }
+  return 0;
+}
+
+void sendMessages() {
+  if(messageSerialOut[0] != '\0') {
+    Serial.write("client:\"");
+    Serial.write(messageSerialOut);
+    Serial.write("\"\r\n");
+  }
+
+  if(messageWiFiOut[0] != '\0') {
+    client.write("serial:\"");
+    client.write(messageWiFiOut);
+    client.write("\"\r\n");
+  }
 }
 
 void setupWiFi()
 {
   WiFi.hostname(domain);
-  WiFi.begin(ssid,password);
+  WiFi.begin(ssid, password);
   //Wait for connection
-  while(WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
 #if DEBUG
     Serial.print(".");
--- microcontroller/esp8266/settings.h
+++ microcontroller/esp8266/settings.h
@@ -2,9 +2,9 @@
 #define SETTINGS_H
 
 // Hardcode WiFi parameters as this isn't going to be moving around.
-const char* domain = "";
-const char* ssid = "";
-const char* password = "";
-const int   port = 100;
+const char* domain = "esp8266-light";
+const char* ssid = "abwifi";
+const char* password = "ABASMASB1234";
+const int   port = 5045;
 
 #endif
