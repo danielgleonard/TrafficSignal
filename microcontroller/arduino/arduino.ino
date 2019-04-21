51c545873ba2029d1ea05382dbbb4a6b383c13f4
Dan Leonard <daniel@danleonard.us>
2019-04-20 21:30:10 -0500

    beep boop

--- microcontroller/arduino/arduino.ino
+++ microcontroller/arduino/arduino.ino
@@ -1,24 +1,57 @@
 #include "light.h"
 
-Light lightGreen(2);
-Light lightYellow(3);
-Light lightRed(4);
+#define MAX_INPUT 32
 
-void setup() {
-  /*
-  lightGreen  = new Light(2);
-  lightYellow = new Light(3);
-  lightRed    = new Light(4);
-  */
-  
+boolean lights[(byte)3];
+const byte green = 0;
+const byte yellow = 1;
+const byte red = 2;
+
+void setup() {  
   Serial.begin(57600);
-  
-  // Send test pattern
-//  lightDispTest();
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    Serial.print(Serial.read());
+  while (Serial.available () > 0) {
+    processIncomingByte(Serial.read ());
   }
 }
+
+interpret(char* msg) {
+  
+}
+
+/**
+ * Process the next byte coming in
+ * 
+ * Adapted from a forum post by 'Nick Gammon'
+ * available at http://www.gammon.com.au/serial
+ * 
+ * @param inByte The byte coming in from Serial
+ */
+void processIncomingByte (const byte inByte) {
+  static char input_line[MAX_INPUT];
+  static unsigned int input_pos = 0;
+  
+  switch (inByte) {
+    case '\n':   // end of text
+      input_line[input_pos] = 0;  // terminating null byte
+      
+      // terminator reached! process input_line here ...
+      interpret(input_line);
+      
+      // reset buffer for next time
+      input_pos = 0;  
+      break;
+  
+    case '\r':   // discard carriage return
+      break;
+  
+    default:
+      // keep adding if not full ... allow for terminating null byte
+      if (input_pos < (MAX_INPUT - 1))
+        input_line[input_pos++] = inByte;
+      break;
+  }  // end of switch
+ 
+} // end of processIncomingByte  
